c42de4162a88e290e688987898965d0a4809a28f
Mohammad Waseem M <mohammedwaseem.m786@gmail.com>
2025-08-20 19:17:37 +0530

    updated App.js removed nested navigation which was already wrapped in AppNavigator.js

--- App.js
+++ App.js
@@ -5,8 +5,6 @@ import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App() {
   return (
-    //<NavigationContainer>
       <AppNavigator />
-    //</NavigationContainer>
   );
 }
